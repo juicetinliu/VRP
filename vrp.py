73b8542f3cd4753063381cba38f4b51f7fec3ba4
juicetinliu <37272242+juicetinliu@users.noreply.github.com>
2020-12-17 22:39:06 -0500

    Update

--- vrp.py
+++ vrp.py
@@ -2,7 +2,10 @@ import xml.etree.ElementTree as ET
 import math,copy
 
 # CREATING THE INSTANCE FILE
-vrp_source = 'A/A-n32-k5'
+# vrp_source = 'A/A-n32-k5'
+vrp_source = 'B/B-n31-k5'
+# vrp_source = 'P/P-n40-k5'
+# vrp_source = 'B/B-n51-k7'
 
 nodes = []
 requests = []
@@ -76,7 +79,7 @@ def generate_instance_xml():
 
     data_xml = ET.tostring(instance) 
     
-    with open("VRP.xml", "wb") as f: 
+    with open("xml/" + vrp_source.split("/")[1] + ".xml", "wb") as f: 
         f.write(data_xml) 
 
 
@@ -106,7 +109,10 @@ def baseline_sol_to_text():
     vrp_file.close()
 
 def convert_array_to_text():
+    # sol = [0, 12, 1, 13, 7, 16, 0, 14, 22, 9, 8, 11, 4, 28, 18, 6, 26, 0, 20, 5, 25, 10, 15, 29, 27, 0, 21, 31, 19, 17, 2, 3, 23, 0, 24, 30, 0]
     sol = [0, 3, 1, 19, 24, 11, 15, 14, 0, 4, 29, 22, 23, 12, 8, 0, 5, 25, 18, 16, 21, 0, 6, 9, 17, 13, 30, 7, 0, 20, 27, 10, 2, 26, 28, 0]
+    # sol = [0, 1, 22, 20, 35, 36, 3, 28, 31, 26, 8, 0, 4, 19, 13, 25, 14, 24, 23, 7, 0, 9, 10, 33, 39, 30, 34, 21, 29, 16, 32, 0, 11, 2, 38, 5, 37, 15, 17, 12, 0, 18, 6, 27, 0]
+    # sol = [0, 4, 43, 11, 6, 48, 3, 19, 33, 0, 5, 36, 40, 16, 34, 38, 2, 10, 17, 0, 7, 26, 28, 14, 42, 13, 0, 8, 12, 37, 32, 31, 27, 41, 0, 9, 49, 30, 24, 22, 46, 25, 0, 15, 50, 23, 47, 45, 0, 18, 39, 29, 0, 21, 44, 20, 1, 35, 0]
     path = ""
     for n in sol:
         path += str(n) + " "
